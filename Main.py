729bc989ba894fc75a987da19a088874a208c07e
florian <florian.barrois25@orange.fr>
2014-03-20 10:19:03 +0100

    update main

--- Main.py
+++ Main.py
@@ -10,6 +10,8 @@ all_vie = []
 
 for i in range(1,capteurs+1) :
 
+	print "\n\n### Capteur",i,"###	 "
+
 	val_zones = input("Entrez les zones : ")
 	while not(isinstance(val_zones,list)) :
 		print "Erreur premier paramètre : liste demandée."
@@ -24,29 +26,12 @@ for i in range(1,capteurs+1) :
 
 	all_capteurs.append(capt)
 
-	for obj in all_capteurs :
-		print "\n\n###Capteur ",all_capteurs.index(obj)+1," ### "
-		print "Zones : ",obj.zones
-		print "Durée de vie : ",obj.vie
+for obj in all_capteurs :
+	print "\n\n### Capteur",all_capteurs.index(obj)+1,"###"
+	print "Zones : ",obj.zones
+	print "Durée de vie : ",obj.vie
 
 
-	#print all_capteurs
-# 	i.zones = input("Entrez les zones : ")
-# 	while not(isinstance(i.zones,list)) :
-# 		print "Erreur premier paramètre : liste demandée."
-# 		i.zones = input("Entrez les zones : ")
-
-# 	i.vie = input("Entrez la durée de vie : ")
-# 	while not(isinstance(i.vie,list)) :
-# 		print "Erreur premier paramètre : liste demandée."
-# 		i.vie = input("Entrez la durée de vie : ")
-
-# 	all_zones.append(i.zones)
-# 	all_vie.append(i.vie)
-
-
-# print all_zones
-# print all_vie
 
 
 
